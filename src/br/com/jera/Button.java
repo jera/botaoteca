d597df1b5b53edfe188b6d2804fd646a73f94be4
marcosbeirigo <marcosbeirigo@gmail.com>
2010-10-21 11:20:22 -0300

    criado mecanismo para tocar multiplos sons

--- src/br/com/jera/Button.java
+++ src/br/com/jera/Button.java
@@ -4,28 +4,51 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 
+import android.content.Context;
 import android.media.MediaPlayer;
+import android.os.Environment;
+import android.view.View;
 
 //TODO talvez o Botão da aplicação deva extender um android.widget.Button 
-public class Button{
-	
-	private static final MediaPlayer PLAYER = new MediaPlayer();
-	//TODO setar path correto para os arquivos
-	private static final String PATH = "/sdcard/sounds";
-	
-	public Button(String name) {
+public class Button extends android.widget.Button {
+
+	private static MediaPlayer PLAYER = new MediaPlayer();
+	// TODO setar path correto para os arquivos
+	public static final String PATH = Environment.getExternalStorageDirectory()+File.separator+"sounds";
+
+	public Button(String name, Context context) {
+		super(context);
 		this.name = name;
+		this.setOnClickListener(new Action());
 	}
-	
+
 	private String name;
-	
-	
-	public void play() throws IllegalArgumentException, IllegalStateException, IOException{
-		File file = new File(PATH+File.separator+name);
-	    FileInputStream fis = new FileInputStream(file);
+
+	public void play() throws IllegalArgumentException, IllegalStateException, IOException {
+		PLAYER.reset();
+		File file = new File(PATH + File.separator + name);
+		FileInputStream fis = new FileInputStream(file);
 		PLAYER.setDataSource(fis.getFD());
 		PLAYER.prepare();
 		PLAYER.start();
 	}
 	
+	private class Action implements OnClickListener{
+		@Override
+		public void onClick(View v) {
+			try {
+				Button.this.play();
+			} catch (IllegalArgumentException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IllegalStateException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
 }
--- src/br/com/jera/Principal.java
+++ src/br/com/jera/Principal.java
@@ -2,24 +2,30 @@ package br.com.jera;
 
 import java.io.File;
 
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 public class Principal extends Activity {
 
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-		updateSongList();
+		File home = new File(Button.PATH);
+		File files[] = home.listFiles();
+		
+		
+		LinearLayout l = (LinearLayout)findViewById(R.id.area);
+		for (File f: files) {
+			Button b = new Button(f.getName(), getApplicationContext());
+			b.setText(f.getName());
+			b.setPadding(1, 1, 2, 0);
+			l.addView(b);
+		}
+		
 	}
 
-	public void updateSongList() {
-		File home = new File("/sdcard/sounds");
-		if (home.listFiles().length > 0) {
-			for (File f : home.listFiles()) {
-				System.out.println(f.getName());
-			}
-		}
-	}
 }
