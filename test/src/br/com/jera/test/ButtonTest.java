1aeffa67668ab4ef86a9e473073b0e5934c39636
marcosbeirigo <marcosbeirigo@gmail.com>
2010-10-29 16:41:56 -0300

    teste para o método play da classe Button

--- test/src/br/com/jera/test/ButtonTest.java
+++ test/src/br/com/jera/test/ButtonTest.java
@@ -1,8 +1,26 @@
 package br.com.jera.test;
 
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.LinearLayout;
+import br.com.jera.Principal;
 
-public class ButtonTest {
+import com.jayway.android.robotium.solo.Solo;
+
+public class ButtonTest extends ActivityInstrumentationTestCase2<Principal>{
+	
+	private Solo solo;
+	
+	public ButtonTest() {
+		super(Principal.class);
+	}
+	
+	 public void setUp() throws Exception {
+	        solo = new Solo(getInstrumentation(), getActivity());
+	  }
 	
 	
+	public void testPlay(){
+		solo.clickOnButton("bichona.mp3");
+	}
 	
 }
