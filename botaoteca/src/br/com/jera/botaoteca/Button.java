7d3327de7d98a1f2496b894cc7c0786c6bef4157
Marcos Beirigo <marcosbeirigo@gmail.com>
2010-11-04 14:46:58 -0300

    criação da classe UI Factory para gerar componentes de interface

--- botaoteca/src/br/com/jera/botaoteca/Button.java
+++ botaoteca/src/br/com/jera/botaoteca/Button.java
@@ -8,7 +8,6 @@ import android.graphics.drawable.StateListDrawable;
 import android.view.View;
 import android.view.ViewGroup.LayoutParams;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import br.com.jera.botaoteca.sound.Sound;
 
 public class Button extends android.widget.Button {
