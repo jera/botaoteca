d733a3d0b9e49f04098ce2ac7d1b9f65b5831df2
Marcos Beirigo <marcosbeirigo@gmail.com>
2010-12-22 19:47:56 -0300

    correção no noma da aplicação

--- botaoteca/gen/br/com/jera/botaoteca/R.java
+++ botaoteca/gen/br/com/jera/botaoteca/R.java
@@ -77,8 +77,7 @@ public final class R {
         public static final int widget_yellow=0x7f03000a;
     }
     public static final class string {
-        public static final int app_name=0x7f060001;
-        public static final int hello=0x7f060000;
+        public static final int app_name=0x7f060000;
     }
     public static final class style {
         public static final int button=0x7f070000;
