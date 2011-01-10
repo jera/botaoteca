bd5410004130644a2573b41d83fbc5cbfa49858d
Jonas Alves <jonasfa@gmail.com>
2011-01-10 21:14:48 -0200

    removendo herança desnecessária de Button

--- botaoteca/src/br/com/jera/botaoteca/Button.java
+++ botaoteca/src/br/com/jera/botaoteca/Button.java
@@ -1,47 +1,18 @@
 package br.com.jera.botaoteca;
 
-import java.io.IOException;
-
 import android.content.Context;
-import android.content.res.Resources;
-import android.graphics.drawable.StateListDrawable;
-import android.view.View;
-import android.view.ViewGroup.LayoutParams;
-import android.widget.LinearLayout;
 import br.com.jera.botaoteca.sound.Sound;
 
-public class Button extends android.widget.Button {
-
+public class Button {
 	private String name;
 	private ButtonColor color;
 	private Sound sound;
-	private Context context;
-	private static LinearLayout.LayoutParams params;
 	
 	public Button(ButtonColor color, Context context, Sound sound) {
-		super(context);
-
 		this.color = color;
 		this.sound = sound;
-		this.context  = context;
-		params = new LinearLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
-		
-		this.setOnClickListener(new Action());
-		this.updateViewProperties();
 	}
 	
-	public void updateViewProperties(){
-		
-		Resources resources = context.getResources();
-		StateListDrawable drawable = new StateListDrawable();
-		drawable.addState( PRESSED_ENABLED_STATE_SET , resources.getDrawable(color.getPressed()));
-		drawable.addState( EMPTY_STATE_SET , resources.getDrawable(color.getNormal()));
-		drawable.addState( SELECTED_WINDOW_FOCUSED_STATE_SET , resources.getDrawable(color.getNormal()));
-		this.setBackgroundDrawable(drawable);
-		
-		setLayoutParams(params);
-	}
-
 	public String getName() {
 		return name;
 	}
@@ -54,23 +25,6 @@ public class Button extends android.widget.Button {
 		this.color = color;
 	}
 
-	private class Action implements OnClickListener {
-
-		@Override
-		public void onClick(View v) {
-			try {
-				Button.this.sound.play();
-			} catch (IllegalArgumentException e) {
-				e.printStackTrace();
-			} catch (IllegalStateException e) {
-				e.printStackTrace();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-		
-	}
-
 	public void setName(String name) {
 	    this.name = name;
 	}
@@ -78,6 +32,4 @@ public class Button extends android.widget.Button {
 	public Sound getSound() {
 	    return sound;
 	}
-	
-
 }
