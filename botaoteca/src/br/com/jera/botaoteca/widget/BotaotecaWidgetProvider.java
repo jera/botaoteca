9567755ec93842b6c0b8b67db0e58dae366964be
marcosbeirigo <marcosbeirigo@gmail.com>
2011-02-28 12:42:17 -0400

    correção de problema no widget ao rebootar aparelho

--- botaoteca/src/br/com/jera/botaoteca/widget/BotaotecaWidgetProvider.java
+++ botaoteca/src/br/com/jera/botaoteca/widget/BotaotecaWidgetProvider.java
@@ -1,11 +1,15 @@
 package br.com.jera.botaoteca.widget;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.util.Log;
+import android.widget.RemoteViews;
 import br.com.jera.botaoteca.Botao;
 import br.com.jera.botaoteca.ButtonColor;
 import br.com.jera.botaoteca.R;
@@ -42,6 +46,7 @@ public class BotaotecaWidgetProvider extends AppWidgetProvider {
 
     @Override
     public void onReceive(Context context, Intent intent) {
+	Log.i("WIDGET", "ON_RECIEVE");
 	if (intent.getAction().equals(ACTION_WIDGET_RECEIVER)) {
 	    int appWidgetId = intent.getIntExtra(
 		    AppWidgetManager.EXTRA_APPWIDGET_ID,
@@ -63,5 +68,38 @@ public class BotaotecaWidgetProvider extends AppWidgetProvider {
 	}
 	super.onReceive(context, intent);
     }
-    
+
+    public void onEnabled(Context context) {
+	PackageManager pm = context.getPackageManager();
+	pm.setComponentEnabledSetting(new ComponentName(
+		"br.com.jera.botaoteca", ".widget.BotaotecaWidgetConfigure"),
+		PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
+		PackageManager.DONT_KILL_APP);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager,int[] appWidgetIds) {
+	 SharedPreferences prefs = context.getSharedPreferences(BotaotecaWidgetConfigure.PREFS_NAME, 0);
+	for(int id: appWidgetIds){
+	    	if(prefs.contains(BotaotecaWidgetConfigure.WIDGET_NAME+id)) {
+        	    	String color = prefs.getString(BotaotecaWidgetConfigure.WIDGET_COLOR+id, null); 
+                	RemoteViews views = new RemoteViews(context.getPackageName(),getResourceId(ButtonColor.valueOf(color)));
+                	views.setTextViewText(R.id.widget_title,prefs.getString(BotaotecaWidgetConfigure.WIDGET_NAME+id, null));
+                
+                	Intent intent = new Intent(context, BotaotecaWidgetProvider.class);
+                	intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, id);
+                	intent.setAction(BotaotecaWidgetProvider.ACTION_WIDGET_RECEIVER);
+                
+                	PendingIntent actionPendingIntent = PendingIntent.getBroadcast(context,id, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+                	views.setOnClickPendingIntent(R.id.widget_button, actionPendingIntent);
+                	appWidgetManager.updateAppWidget(id, views);
+	    	}
+	}
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+	Log.i("WIDGET", "ON_DISABLED");
+	super.onDisabled(context);
+    }
 }
