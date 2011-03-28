8f61b1ae323fa8efdb3062b08fe60a4be9634ab9
marcosbeirigo <marcosbeirigo@gmail.com>
2011-03-28 14:39:15 -0400

    otimização da listagem

--- botaoteca/src/br/com/jera/botaoteca/BotaotecaListAdapter.java
+++ botaoteca/src/br/com/jera/botaoteca/BotaotecaListAdapter.java
@@ -8,7 +8,7 @@ import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class BotaotecaListAdapter extends ArrayAdapter<AppButton> {
 
@@ -19,16 +19,29 @@ public class BotaotecaListAdapter extends ArrayAdapter<AppButton> {
 	
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
-		LinearLayout itemLayout = (LinearLayout) super.getView(position, convertView, parent);
 
 		final AppButton button = getItem(position);
+		ViewHolder holder;
+		if(convertView == null) {
+			holder = new ViewHolder();
+			convertView = View.inflate(getContext(), R.layout.gridview_item, null);
+			holder.button = (Button) convertView.findViewById(R.id.gridview_button);
+			holder.text =  (TextView)  convertView.findViewById(R.id.gridview_title);
+			convertView.setTag(holder);
+		}else {
+			holder = (ViewHolder)convertView.getTag();
+		}
+		holder.button.setOnClickListener(onClickListener);
+		holder.button.setBackgroundDrawable(button.getColor().getDrawable(getContext()));
+		holder.button.setTag(button);
+		holder.text.setText(button.getName());
 
-		Button nButton = (Button) itemLayout.findViewById(R.id.gridview_button);
-		nButton.setTag(button);
-		nButton.setOnClickListener(onClickListener);
-		nButton.setBackgroundDrawable(button.getColor().getDrawable(getContext()));
-
-		return itemLayout;
+		return convertView;
+	}
+	
+	private static class ViewHolder {
+		Button button;
+		TextView text;
 	}
 
 	private OnClickListener onClickListener = new OnClickListener() {
