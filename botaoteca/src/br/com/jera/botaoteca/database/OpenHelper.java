193312986da5e8e23787d83a0d56a28aa26c744d
JEFFMOR <jeffmor@gmail.com>
2011-04-15 14:02:27 -0400

    Renomeando arquivos para seguir o padrão

--- botaoteca/src/br/com/jera/botaoteca/database/OpenHelper.java
+++ botaoteca/src/br/com/jera/botaoteca/database/OpenHelper.java
@@ -33,19 +33,22 @@ class OpenHelper extends SQLiteOpenHelper {
 
 	private void populateDatabase(SQLiteDatabase db) {
 		db.beginTransaction();
-		db.execSQL("INSERT INTO sounds VALUES ('pedrochip.mp3','Me dá meu Chipe',1,'RED')");
-		db.execSQL("INSERT INTO sounds VALUES ('grilo.mp3','Grilo',1,'GREEN')");
-		db.execSQL("INSERT INTO sounds VALUES ('hmboiola.mp3','Hm Boiola',1,'GREEN')");
-		db.execSQL("INSERT INTO sounds VALUES ('serracomedor.mp3','Serra Comedor',1,'RED')");
-		db.execSQL("INSERT INTO sounds VALUES('tudumpa.mp3','Tudum pa',1,'ORANGE')");
-		db.execSQL("INSERT INTO sounds VALUES ('valenada.mp3','Voce não vale nada',1,'GREEN')");
-		db.execSQL("INSERT INTO sounds VALUES ('atoron.mp3','Adoro Perigo',1,'YELLOW')");
-		db.execSQL("INSERT INTO sounds VALUES ('vera.mp3','Vera Verão',1,'GREEN')");
-		db.execSQL("INSERT INTO sounds VALUES ('ritmodefesta.mp3','Ritmo de Festa',1,'BLUE')");
-		db.execSQL("INSERT INTO sounds VALUES ('ronaldo.mp3','Ronaldo',1,'BLUE')");
-		db.execSQL("INSERT INTO sounds VALUES ('hojenao.mp3','Cleber Machado',1,'GREEN')");
-		db.execSQL("INSERT INTO sounds VALUES ('jeremias.mp3','Jeremias',1,'RED')");
-		db.execSQL("INSERT INTO sounds VALUES ('travesti.mp3','Travesti',1,'BLUE')");
+		db.execSQL("INSERT INTO sounds VALUES ('Pedro_chip_RED.mp3','Me dá meu Chipe',1,'RED')");
+		db.execSQL("INSERT INTO sounds VALUES ('Grilo_GREEN.mp3','Grilo',1,'GREEN')");
+		db.execSQL("INSERT INTO sounds VALUES ('Tudumpa_ORANGE.mp3','Tudum pa',1,'ORANGE')");
+
+		db.execSQL("INSERT INTO sounds VALUES ('Hum_boiola_GREEN.mp3','Hm Boiola',1,'GREEN')");
+		db.execSQL("INSERT INTO sounds VALUES ('Adoro_Perigo_YELLOW.mp3','Adoro Perigo',1,'YELLOW')");
+		db.execSQL("INSERT INTO sounds VALUES ('Ritmo_de_Festa_BLUE.mp3','Ritmo de Festa',1,'BLUE')");
+
+		db.execSQL("INSERT INTO sounds VALUES ('Vera_Verão_GREEN.mp3','Vera Verão',1,'GREEN')");
+		db.execSQL("INSERT INTO sounds VALUES ('Sou_foda_2_BLUE','Sou foda 2',1,'BLUE')");
+		db.execSQL("INSERT INTO sounds VALUES ('Cleber_Machado_GREEN.mp3','Cleber Machado',1,'GREEN')");
+
+		db.execSQL("INSERT INTO sounds VALUES ('Travesti_BLUE.mp3','Travesti',1,'BLUE')");
+		db.execSQL("INSERT INTO sounds VALUES ('Jeremias_RED.mp3','Jeremias',1,'RED')");
+		db.execSQL("INSERT INTO sounds VALUES ('Ronaldo_BLUE.mp3','Ronaldo',1,'BLUE')");
+
 		db.setTransactionSuccessful();
 		db.endTransaction();
 	}
